5f0a26974ae285e2048582c484fc505e4e67392a
aouali <aghiles.ouali@nassidj.com>
2023-04-28 23:47:33 +0100

    deplacement et ajout de fichier et structure de dossier

---
+++ src/libs/data.js
@@ -0,0 +1,106 @@
+const imageUrl = new URL(
+  '../img/category-thumbnails/marketing/heroes.png',
+  import.meta.url
+)
+
+
+const data =
+  {
+    "components": {
+      "marketing": {
+        "sections": [ {
+            "src": imageUrl,
+            "href": "./components/marketing/sections/heros",
+            "title": "heros",
+            "nbr": "9"
+          },
+          {
+            "src":"img/category-thumbnails/marketing/feature-sections.png",
+            "href": "./components/marketing/sections/feature-sections",
+            "title": "feature-sections",
+            "nbr": "10"
+          },
+          {
+            "src":"img/category-thumbnails/marketing/cta-sections.png",
+            "href": "./components/marketing/sections/cta-sections",
+            "title": "cta-sections",
+            "nbr": "8"
+          },
+          {
+            "src":"img/category-thumbnails/marketing/pricing.png",
+            "href": "./components/marketing/sections/pricing",
+            "title": "pricing",
+            "nbr": "9"
+          },
+          {
+            "src":"img/category-thumbnails/marketing/header.png",
+            "href": "./components/marketing/sections/header",
+            "title": "header",
+            "nbr": "5"
+          },
+          {
+            "src":"img/category-thumbnails/marketing/newsletter-sections.png",
+            "href": "./components/marketing/sections/newsletter-sections",
+            "title": "newsletter-sections",
+            "nbr": "6"
+          },
+          {
+            "src":"img/category-thumbnails/marketing/faq-sections.png",
+            "href": "./components/marketing/sections/faq-sections",
+            "title": "faq-sections",
+            "nbr": "9"
+          },
+          {
+            "src":"img/category-thumbnails/marketing/stats-sections.png",
+            "href": "./components/marketing/sections/stats-sections",
+            "title": "stats-sections",
+            "nbr": "4"
+          },
+          {
+            "src":"img/category-thumbnails/marketing/testimonials.png",
+            "href": "./components/marketing/sections/testimonials",
+            "title": "testimonials",
+            "nbr": "5"
+          },
+          {
+            "src":"img/category-thumbnails/marketing/blog-sections.png",
+            "href": "./components/marketing/sections/blog-sections",
+            "title": "blog-sections",
+            "nbr": "3"
+          },
+          {
+            "src":"img/category-thumbnails/marketing/contact-sections.png",
+            "href": "./components/marketing/sections/contact-sections",
+            "title": "contact-sections",
+            "nbr": "5"
+          },
+          {
+            "src":"img/category-thumbnails/marketing/team-sections.png",
+            "href": "./components/marketing/sections/team-sections",
+            "title": "team-sections",
+            "nbr": "8"
+          },
+          {
+            "src":"img/category-thumbnails/marketing/content-sections.png",
+            "href": "./components/marketing/sections/content-sections",
+            "title": "content-sections",
+            "nbr": "6"
+          },
+          {
+            "src":"img/category-thumbnails/marketing/footers.png",
+            "href": "./components/marketing/sections/footers",
+            "title": "footers",
+            "nbr": "7"
+          },
+          {
+            "src":"img/category-thumbnails/marketing/logo-clouds.png",
+            "href": "./components/marketing/sections/logo-clouds",
+            "title": "logo-clouds",
+            "nbr": "5"
+          }]
+      }
+    }
+  }
+
+
+module.exports = data
