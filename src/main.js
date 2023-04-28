a007e64bab297f92e4251fb0dd0ca762e831ff7a
aouali <aghiles.ouali@nassidj.com>
2023-04-28 22:57:18 +0100

    ajout des js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import './style.css'
+import './libs/components'
+import Alpine from 'alpinejs'
+//import collapse from '@alpinejs/collapse'
+document.addEventListener('alpine:init', () => {
+    Alpine.data('dropdown', () => ({
+      open: false,
+      toggle () {
+          this.open = !this.open
+        }
+    }))
+
+})
+//Alpine.plugin(collapse)
+Alpine.start()
